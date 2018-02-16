7b8c93e9cf14b04acaff5c1edce1444de8511fe0
eduardoGo <egsantos128@gmail.com>
2018-02-16 16:07:28 -0300

    Add files via upload

---
+++ SistemaPagamento/src/CartaoPonto.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class CartaoPonto {
+
+    private int identificacao;
+
+    private ArrayList<DataPontos> dataPontos = new ArrayList<DataPontos>();
+
+    public CartaoPonto(int identificacao){
+
+        this.identificacao = identificacao;
+    }
+
+    public ArrayList<DataPontos> getDataPontos() {
+
+        return dataPontos;
+    }
+}
+
---
+++ SistemaPagamento/src/DataPontos.java
@@ -0,0 +1,25 @@
+public class DataPontos {
+
+    private Date data;
+    private int horaChegada;
+    private int horaSaida;
+
+    public DataPontos(Date data,int horaChegada,int horaSaida){
+
+        this.data = data;
+        this.horaChegada = horaChegada;
+        this.horaSaida = horaSaida;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public int getHoraChegada() {
+        return horaChegada;
+    }
+
+    public int getHoraSaida() {
+        return horaSaida;
+    }
+}
